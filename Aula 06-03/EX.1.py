9f916ce4a16153486b5a4caf99b8ed2027c6f8eb
Pedro Henrique Buzutti Telli <1056191@labcomp.usc>
2024-03-13 19:17:30 -0300

    lista 06-03

---
+++ Aula 06-03/EX.1.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify
+import json
+import os
+
+app = Flask(__name__)
+arquivo_usuarios = 'usuarios.json'
+
+def load_usuarios():
+    if os.path.exists(arquivo_usuarios):
+        with open(arquivo_usuarios, 'r') as file:
+            return json.load(file)
+    else:
+        return []
+
+@app.route('/')
+def index():
+    return 'Bem-vindo à API da lista 3!'
+
+
+@app.route('/calc', methods=['GET'])
+def calc():
+    num1 = request.args.get('num1', type=float)
+    num2 = request.args.get('num2', type=float)
+    op = request.args.get('op')
+
+    if op == '+':
+        result = num1 + num2
+    elif op == '-':
+        result = num1 - num2
+    elif op == '*':
+        result = num1 * num2
+    elif op == '/':
+        result = num1 / num2
+    return jsonify({'result': result})
+
+#Crie uma API para cadastro de usuários, permitindo a inclusão, consulta, atualização e exclusão de usuários.
+#Obs: Utilize arquivos (txt ou JSON) para simular cada operação de persistência de dados.
+@app.route('/usuarios', methods=['GET'])
+def get_usuarios():
+    users = load_usuarios()
+    return jsonify(users)
+
+if __name__ == '__main__':
+    app.run(debug=True)
